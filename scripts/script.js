3e48d149537a7516c29c8580f8ca01e1fe98e667
JungTzen <jtliew@ucsd.edu>
2023-11-26 18:59:45 -0800

    partial hw3

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,55 @@
 const form_name = document.getElementById("form_name");
-const email = document.getElementById("email");
-const comments = document.getElementById("comments");
+const form = document.querySelector("form");
+const nameError = document.getElementById("name_error");
+const nameRegExp = /^[A-Za-z]+$/;
 
-form_name.addEventListener("input", (event) => {
-    if (form_name.validity.valueMissing){
-        form_name.setCustomValidity("Please enter your name :)");
-    }
-    else if (form_name.validity.patternMismatch) {
+
+form_name.addEventListener("input", () => {
+    const isValid = form_name.value.length === 0 || nameRegExp.test(form_name.value);
+    form_name.className = isValid ? "valid" : "invalid";
+    nameError.textContent = isValid ? "" : "Please enter your name :)";
+    if(!isValid){
         form_name.setCustomValidity("Make sure your input only contains letters and spaces.");
-    } 
-    else {
+    }
+    else{
         form_name.setCustomValidity("");
     }
+
+    nameError.textContent = isValid ? "" : "Invalid input";
   });
+
+window.addEventListener("load", () => {
+  const isValid = form_name.value.length === 0 || nameRegExp.test(form_name.value);
+  form_name.className = isValid ? "valid" : "invalid";
+});
+
+form_name.addEventListener("input", () => {
+  const isValid = form_name.value.length === 0 || nameRegExp.test(email.value);
+  
+  if (isValid) {
+    form_name.className = "valid";
+    nameError.textContent = "";
+    nameError.className = "error";
+  } 
+  else {
+    form_name.className = "invalid";
+  }
+});
+
+
+
+
+/*
+
+const email = document.getElementById("email");
+const comments = document.getElementById("comments");
 
 email.addEventListener("input", (event) => {
     if (email.validity.valueMissing){
         email.setCustomValidity("Please enter your email :)");
     }
     else if (email.validity.typeMismatch) {
-      email.setCustomValidity("Make sure this is a proper email with the @ symbol");
+      email.setCustomValidity("Make sure this is a proper email with an @ symbol included");
     } 
     else {
       email.setCustomValidity("");
@@ -37,3 +67,6 @@ comments.addEventListener("input", (event) => {
         comments.setCustomValidity("");
     }
   });
+
+
+*/
